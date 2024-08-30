67215f01c2ec151e7a2bdeeffc2873052160d3f8
hungphitran <hungphitran.22@gmail.com>
2024-08-30 10:16:57 +0700

    show-detailed-helper

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -4,6 +4,7 @@ const dashboardController={
     show: async (req,res,next)=>{
         let locations;
         let services;
+        let helpers;
             try{
                 //call api to get locations
                locations= await fetch(process.env.API_URL+'/location')
@@ -14,10 +15,22 @@ const dashboardController={
             catch(err){
                 console.error(err);
             }
+
+            try{
+                //call api to get helpers
+               helpers= await fetch(process.env.API_URL+'/helper')
+               .then(data=>data.json())
+               .then(data=>data)
+               
+            }
+            catch(err){
+                console.error(err);
+            }
         // give data to dashboard
         res.render('partials/index',{
             locations:locations,
-            services:services
+            services:services,
+            helpers:helpers
         })
     }
 }
--- controllers/detailedhelperController.js
+++ controllers/detailedhelperController.js
@@ -1,8 +1,23 @@
 require('dotenv').config()
 const detailedhelperController={
-    show:(req,res,next)=>{
-        let id=req.body.id;
-        res.render('partials/detailedhelper')
+    show:async (req,res,next)=>{
+        let id=req.params.id;
+        let helper;
+        try{
+            //call api to get locations
+           helper= await fetch(process.env.API_URL+`/helper/${id}`)
+           .then(data=>data.json())
+           .then(data=>data)
+           .catch(err=>console.error(err))
+           
+        }
+        catch(err){
+            console.error(err);
+        }
+        helper.birthDate=helper.birthDate.slice(0,10);
+        res.render('partials/detailedhelper',{
+            helper:helper
+        })
     }
 }
 
