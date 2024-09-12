cb71ac0a39b990eb5de022e408bda351461139c0
hung tommy <doviethung.5409@gmail.com>
2024-09-12 09:17:57 +0700

    update the change in the folder

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -3,17 +3,18 @@ const { submit } = require("./longtermController")
 const accountController={
     //show login page
     showLogin: async (req,res,next)=>{
-        res.render('partials/login')
+        // res.render('partials/login')
+        res.render('messages/error',{layout: false})
     },
         //show login page
-
     showRegister: async (req,res,next)=>{
         res.render('partials/register')
     },
     //login and show account
     login: async (req,res,next)=>{
         console.log(req.body)
-        res.render('partials/detailedaccount')
+        // res.render('partials/detailedaccount')
+        res.render('messages/error',{layout: false})
     },
     register: async (req,res,next)=>{
         res.send(req.body)
