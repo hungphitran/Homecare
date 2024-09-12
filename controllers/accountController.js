74a4bf0c873ba7e7d472372ebc8c75bed4ecabf7
hungphitran <hungphitran.22@gmail.com>
2024-09-12 10:16:10 +0700

    change pull request for backend-branch

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -3,8 +3,7 @@ const { submit } = require("./longtermController")
 const accountController={
     //show login page
     showLogin: async (req,res,next)=>{
-        // res.render('partials/login')
-        res.render('messages/error',{layout: false})
+        res.render('partials/login',{layout: false})
     },
         //show login page
     showRegister: async (req,res,next)=>{
@@ -13,7 +12,6 @@ const accountController={
     //login and show account
     login: async (req,res,next)=>{
         console.log(req.body)
-        // res.render('partials/detailedaccount')
         res.render('messages/error',{layout: false})
     },
     register: async (req,res,next)=>{
