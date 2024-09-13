847cd9a89bf18bbabf55af437bd9da577b18b9c0
hung tommy <doviethung.5409@gmail.com>
2024-09-13 17:30:28 +0700

    done the sign-in and sign-up page and validate

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -4,7 +4,7 @@ const accountController={
     //show login page
     showLogin: async (req,res,next)=>{
         // res.render('partials/login')
-        res.render('messages/error',{layout: false})
+        res.render('pages/login',{layout: false})
     },
         //show login page
     showRegister: async (req,res,next)=>{
@@ -12,12 +12,14 @@ const accountController={
     },
     //login and show account
     login: async (req,res,next)=>{
-        console.log(req.body)
+        // console.log(req.body)
         // res.render('partials/detailedaccount')
-        res.render('messages/error',{layout: false})
+        res.render('pages/login',{layout: false})
     },
     register: async (req,res,next)=>{
-        res.send(req.body)
+        // res.send(req.body)
+        // console.log(req.body);
+        res.redirect('/account');
     }
 
 
