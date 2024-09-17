91b2d2554bedcaf9d8b7090bcaa2eea6926fdd54
hungphitran <hungphitran.22@gmail.com>
2024-09-17 18:20:34 +0700

    login available

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,27 +1,33 @@
-const { submit } = require("./longtermController")
-
-const accountController={
+require('dotenv').config()
+const accountController = {
     //show login page
-    showLogin: async (req,res,next)=>{
+    showLogin: async (req, res, next) => {
 
         // res.render('partials/login')
-        res.render('pages/login',{layout: false});
+        res.render('pages/login', { layout: false });
 
     },
-        //show login page
-    showRegister: async (req,res,next)=>{
-        res.render('pages/register',{layout:false})
+    //show login page
+    showRegister: async (req, res, next) => {
+        res.render('pages/register', { layout: false })
     },
     //login and show account
-    login: async (req,res,next)=>{
+    login: async (req, res, next) => {
+        console.log(req.body)
+        user = await fetch(process.env.API_URL + '/customer/' + req.body.phone)
+            .then(data => data.json())
 
-        // console.log(req.body)
-        // res.render('partials/detailedaccount')
-        // console.log(req.body);
-        res.render('pages/login',{layout: false})
-        // res.send(req.body);
+        if (!user) {
+            alert('no account with phone number: ' + req.phone);
+        }
+        else if (user.password !== req.body.password) {
+            alert('wrong password');
+        }
+        else {
+            res.redirect('/');
+        }
     },
-    register: async (req,res,next)=>{
+    register: async (req, res, next) => {
         // res.send(req.body)
         // console.log(req.body);
         res.redirect('/account');
