aa71cd7a3c315d6f9dc1b5e457c8593524ec1f40
hungphitran <hungphitran.22@gmail.com>
2024-09-23 11:18:21 +0700

    change request route and delete controller for ordering

--- routes/shorttermOrder.route.js
+++
@@ -1,7 +0,0 @@
-const shorttermController =require('../controllers/shorttermController')
-const router =require('express').Router();
-
-router.post('/submit',shorttermController.submit)
-router.post('/',shorttermController.filter);
-
-module.exports=router;
