12d7618c13f1b3b84543cf8b6cf279fe5b983969
hungphitran <hungphitran.22@gmail.com>
2024-10-14 21:25:51 +0700

    add blog page

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,8 +1,46 @@
 require('dotenv').config()
 
+function sortByDate(blogs){
+    return blogs.sort((b1,b2)=>{
+        return b1.date - b2.date;
+    })
+}
+
 const blogController ={
     showAll: async (req,res,next)=>{
+        let blogs=await fetch(process.env.API_URL+'/blog')
+        .then(data=>data.json())
+        .catch(err=>console.error(err))
+        blogs=sortByDate(blogs)
         res.render('pages/blog',{
+            blog:blogs,
+            layout:false
+        })
+    },
+    filter: async(req,res,next)=>{
+        let blogs=await fetch(process.env.API_URL+'/blog')
+        .then(data=>data.json())
+        .catch(err=>console.error(err))
+        let key= req.body.key.toLowerCase()
+
+        blogs=blogs.filter((blog,index)=>{
+            return blog.title.toLowerCase().includes(key)||blog.description.toLowerCase().includes(key)||blog.date.includes(key)
+        })
+        blogs=sortByDate(blogs)
+
+        res.render('pages/blog',{
+            key:key,
+            blog:blogs,
+            layout:false
+        })
+    },
+    showOne: async (req,res,next)=>{
+        let blog=await fetch(process.env.API_URL+"/blog/"+req.params.id)
+        .then(data=>data.json())
+        .catch(err=>console.error(err))
+
+        res.render('pages/detailBlog',{
+            blog:blog,
             layout:false
         })
     }
--- routes/config.route.js
+++ routes/config.route.js
@@ -1,13 +1,13 @@
 const dashboardRoute = require('./dashboard.route')
 const accountRoute = require('./account.route')
 const helperRoute = require('./detailedhelper.route')
-//const accountRoute=require('./detailedaccount.route')
-
+const blogRoute = require('./blog.route')
 const mailRoute = require('./mail.route')
 const requestRoute = require('./request.route');
 const headerLoad = require('../middlewares/headerLoad')
 
 function route(app) {
+    app.use('/blog',blogRoute)
     app.use('/helper', helperRoute)
     app.use('/request', requestRoute)
     app.use('/account', accountRoute)
